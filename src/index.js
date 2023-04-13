ffc72d9bb31c7ae0a7d813f92802c5f0ba7718ab
Justin Duong <jduong@umich.edu>
2023-04-13 14:04:42 -0400

    Added logic to handle createToDo form submit.

--- src/index.js
+++ src/index.js
@@ -39,6 +39,8 @@ function ToDo(title, description, dueDate, priority, notes) {
   this.dueDate = dueDate;
   this.priority = priority;
   this.notes = notes;
+
+  return { title, dueDate };
 }
 
 function Project(name) {
@@ -68,18 +70,104 @@ function Project(name) {
 
 // First interface is Todays ToDos
 
-function ToDoController() {
+/*
+const ToDoController = () => {
   const createTodo = (formData) => {
-    FormController.createTodo(formData);
+    const todo = FormController.createTodo(formData);
+
+    projectToday.addTodo(todo);
+    console.log(projectToday);
   }
 
   const editTodo = (formData) => {
     FormController.editTodo(formData);
   }
+
+  return { createTodo }; 
 }
 
+*/
+
+const createDOM = () => {
+  const divMain = document.createElement('div');
+  divMain.id = 'content';
+  const btnCreateTodo = document.createElement('button');
+  btnCreateTodo.innerHTML = '+';
+
+  btnCreateTodo.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    console.log(e);
+  });
+
+  divMain.append(btnCreateTodo);
+  document.querySelector('body').appendChild(divMain);
+}
+
+const createForm = () => {
+  const divMain = document.getElementById('content');
+  const form = document.createElement('form');
+  form.id = 'form-create-todo';
+
+  const inputs = ['title', 'description', 'due-date', 'priority', 'notes'];
+
+  for (let i = 0; i < inputs.length; i++) {
+    const inputName = inputs[i];
+
+    const label = document.createElement('label');
+    label.setAttribute('for', inputName);
+
+    const input = document.createElement('input');
+    input.id = inputName;
+    input.setAttribute('name', inputName);
+    
+    if (inputName === 'title' || inputName === 'description') {
+      input.type = 'text';
+    } else if (inputName === 'due-date') {
+      input.type = 'date';
+    }
+
+    form.append(label, input);
+  }
+
+  /* Submit input */
+  const btnSubmit = document.createElement('button');
+  btnSubmit.id = 'button-submit';
+  btnSubmit.type = 'submit';
+  btnSubmit.innerHTML = 'Create Todo';
+
+  btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    FormController.createTodo(form.elements);
+  });
+
+  form.appendChild(btnSubmit);
+
+  divMain.append(form);
+}
+// returns a 6 element HTML Collection
+// We iterate to 5, while skipping the last one because that is the submit button.
+const FormController = {
+  createTodo: (formData) => {
+    const newTodo = ToDo(formData[0].value, formData[1].value, formData[2].value, formData[3].value, formData[4].value);
+    projectToday.addTodo(newTodo);
+    DomUpdater.addNewTodo(newTodo);
+  }
+};
+
+const DomUpdater = {
+  addNewTodo: (todo) => {
+    const divContent = document.getElementById('content');
+    
+    const divTodo = document.createElement('div');
+    divTodo.innerHTML = `${todo.title}, ${todo.dueDate}`;
+
+    divContent.append(divTodo);
+  }
+}
 
 // On Script Load, let's do some basic stuff
-const todaysProject = Project('Today');
-todaysProject.addTodo(ToDo('Code', 'Code', '09/09/1996', 'High', 'Code'));
-console.log(todaysProject);
+createDOM();
+createForm();
+const projectToday = Project('today');
