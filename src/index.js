6285cb89edac0918732797512517403c7bea11cb
Justin Duong <jduong@umich.edu>
2023-04-17 16:33:03 -0400

    Changed todo list item inner html. Added two svgs, edit button and delete button. Added some css for todo list item.

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function ToDo(title, description, dueDate, priority, notes) {
   this.priority = priority;
   this.notes = notes;
 
-  return { title, dueDate };
+  return { title, description, dueDate };
 }
 
 function Project(name) {
@@ -90,6 +90,7 @@ const createForm = () => {
       input.id = inputName;
       input.setAttribute('name', inputName);
       input.type = 'text';
+      input.maxLength = 49;
       form.append(label, input);
     } else if (inputName === 'due-date') {
       label.innerHTML = 'Due Date:';
@@ -159,8 +160,44 @@ const DomUpdater = {
     const listTodos = document.getElementById('list-todos');
     
     const listItemTodo = document.createElement('li');
-    listItemTodo.innerHTML = `${todo.title}, ${todo.dueDate}`;
+    listItemTodo.innerHTML = `
+      <div class="todo-information">
+        <h3 class="todo-title">${todo.title}</h3>
+        <span class="todo-description">${todo.description}</h3>
+      </div>
+    `;
+    
+    // Add edit and delete buttons to todo list item.
+    const listButtons = document.createElement('ul');
+    listButtons.classList.add('list-buttons');
 
+    const listItemEdit = document.createElement('li');
+    const xmlns = 'http://www.w3.org/2000/svg';
+    const svgBtnWidth = 18;
+    const svgBtnHeight = 18;
+    const svgEdit = document.createElementNS(xmlns, 'svg');
+    listItemEdit.appendChild(svgEdit);
+    svgEdit.setAttributeNS(null, 'viewBox', '0 0 512 512');
+    svgEdit.setAttributeNS(null, 'width', svgBtnWidth);
+    svgEdit.setAttributeNS(null, 'height', svgBtnHeight);
+
+    const pathEdit = document.createElementNS(xmlns, 'path');
+    svgEdit.appendChild(pathEdit)
+    pathEdit.setAttributeNS(null, 'd', 'M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z');
+
+    const listItemDelete = document.createElement('li');
+    const svgDelete = document.createElementNS(xmlns, 'svg');
+    listItemDelete.appendChild(svgDelete);
+    svgDelete.setAttributeNS(null, 'viewBox', '0 0 448 512');
+    svgDelete.setAttributeNS(null, 'width', svgBtnWidth);
+    svgDelete.setAttributeNS(null, 'height', svgBtnHeight);
+
+    const pathDelete = document.createElementNS(xmlns, 'path');
+    svgDelete.appendChild(pathDelete);
+    pathDelete.setAttributeNS(null, 'd', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
+    
+    listButtons.append(listItemEdit, listItemDelete);
+    listItemTodo.appendChild(listButtons);
     listTodos.append(listItemTodo);
   }
 }
