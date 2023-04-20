52ae5255e2bb0e0ac68b9786e587ed7943ea84e2
Justin Duong <jduong@umich.edu>
2023-04-20 15:39:13 -0400

    extracted creating todolistItem into one function and finished event listener for deleting a todo list item.

--- src/index.js
+++ src/index.js
@@ -73,6 +73,88 @@ function Project(name) {
 
 // formData returns a 6 element HTML Collection
 // We iterate to 5, while skipping the last one because that is the submit button.
+function createTodoListItem(todo) {
+  const todoInformation = todo.getInfo();
+  
+  const listItemTodo = document.createElement('li');
+  listItemTodo.innerHTML = `
+    <div class="todo-information">
+      <h3 class="todo-title">${todoInformation._title}</h3>
+      <span class="todo-description">${todoInformation._description}</span>
+    </div>
+  `;
+
+  listItemTodo.setAttribute('data-uuid', todo.uuid);
+  
+  // Add edit and delete buttons to todo list item.
+  const listButtons = document.createElement('ul');
+  listButtons.classList.add('list-buttons');
+
+  const listItemEdit = document.createElement('li');
+  const btnEdit = document.createElement('button');
+  btnEdit.classList.add('btn-todo');
+  const xmlns = 'http://www.w3.org/2000/svg';
+  const svgBtnWidth = 18;
+  const svgBtnHeight = 18;
+  const svgEdit = document.createElementNS(xmlns, 'svg');
+  btnEdit.appendChild(svgEdit);
+  listItemEdit.appendChild(btnEdit);
+  svgEdit.setAttributeNS(null, 'viewBox', '0 0 512 512');
+  svgEdit.setAttributeNS(null, 'width', svgBtnWidth);
+  svgEdit.setAttributeNS(null, 'height', svgBtnHeight);
+
+  const pathEdit = document.createElementNS(xmlns, 'path');
+  svgEdit.appendChild(pathEdit)
+  pathEdit.setAttributeNS(null, 'd', 'M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z');
+
+  // Add event listener to btnEdit for editing todo.
+  btnEdit.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const divParent = e.currentTarget.parentElement.parentElement.parentElement;
+    const uuid = divParent.getAttribute('data-uuid');
+
+    DomUpdater.editTodo(uuid);
+    DomUpdater.setFormPurpose('edit');
+  });
+
+  const listItemDelete = document.createElement('li');
+  const btnDelete = document.createElement('button');
+  btnDelete.classList.add('btn-todo');
+  const svgDelete = document.createElementNS(xmlns, 'svg');
+  btnDelete.appendChild(svgDelete);
+  listItemDelete.appendChild(btnDelete);
+  svgDelete.setAttributeNS(null, 'viewBox', '0 0 448 512');
+  svgDelete.setAttributeNS(null, 'width', svgBtnWidth);
+  svgDelete.setAttributeNS(null, 'height', svgBtnHeight);
+
+  const pathDelete = document.createElementNS(xmlns, 'path');
+  svgDelete.appendChild(pathDelete);
+  pathDelete.setAttributeNS(null, 'd', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
+  
+  // Add event listener to btnDelete for deleting todo.
+  btnDelete.addEventListener('click', (e) => {
+    // Function for deleting todo.
+    e.preventDefault();
+    // We need to know which todo it is.
+    const divParent = e.currentTarget.parentElement.parentElement.parentElement;
+    const uuid = divParent.getAttribute('data-uuid');
+    // Object holding todos will just delete key, effectively deleting todo.
+    delete todos[uuid];
+    DomUpdater.updateTodoList();
+    // Update Dom, with todo being deleted.
+  });
+
+
+  // Add Expand button
+  // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
+
+  listButtons.append(listItemEdit, listItemDelete);
+  listItemTodo.appendChild(listButtons);
+  
+  return listItemTodo;
+}
+
 const FormController = {
   createTodo: (formData) => {
     const newTodo = ToDo(formData[0].value, formData[1].value, formData[2].value, formData[3].value, formData[4].value);
@@ -94,80 +176,9 @@ const FormController = {
 const DomUpdater = {
   addNewTodo: (todo) => {
     const listTodos = document.getElementById('list-todos');
-    const todoInformation = todo.getInfo();
-    
-    const listItemTodo = document.createElement('li');
-    listItemTodo.innerHTML = `
-      <div class="todo-information">
-        <h3 class="todo-title">${todoInformation._title}</h3>
-        <span class="todo-description">${todoInformation._description}</span>
-      </div>
-    `;
+    const listItemTodo = createTodoListItem(todo);
 
-    listItemTodo.setAttribute('data-uuid', todo.uuid);
-    
-    // Add edit and delete buttons to todo list item.
-    const listButtons = document.createElement('ul');
-    listButtons.classList.add('list-buttons');
-
-    const listItemEdit = document.createElement('li');
-    const btnEdit = document.createElement('button');
-    btnEdit.classList.add('btn-todo');
-    const xmlns = 'http://www.w3.org/2000/svg';
-    const svgBtnWidth = 18;
-    const svgBtnHeight = 18;
-    const svgEdit = document.createElementNS(xmlns, 'svg');
-    btnEdit.appendChild(svgEdit);
-    listItemEdit.appendChild(btnEdit);
-    svgEdit.setAttributeNS(null, 'viewBox', '0 0 512 512');
-    svgEdit.setAttributeNS(null, 'width', svgBtnWidth);
-    svgEdit.setAttributeNS(null, 'height', svgBtnHeight);
-
-    const pathEdit = document.createElementNS(xmlns, 'path');
-    svgEdit.appendChild(pathEdit)
-    pathEdit.setAttributeNS(null, 'd', 'M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z');
-
-    // Add event listener to btnEdit for editing todo.
-    btnEdit.addEventListener('click', (e) => {
-      e.preventDefault();
-
-      const divParent = e.currentTarget.parentElement.parentElement.parentElement;
-      const uuid = divParent.getAttribute('data-uuid');
-
-      DomUpdater.editTodo(uuid);
-      DomUpdater.setFormPurpose('edit');
-
-      // Need to change form to edit mode and populate input values with todo values.
-      // FormController.editTodo(uuid, formEdit.elements);
-    });
-
-
-
-
-    const listItemDelete = document.createElement('li');
-    const btnDelete = document.createElement('button');
-    btnDelete.classList.add('btn-todo');
-    const svgDelete = document.createElementNS(xmlns, 'svg');
-    btnDelete.appendChild(svgDelete);
-    listItemDelete.appendChild(btnDelete);
-    svgDelete.setAttributeNS(null, 'viewBox', '0 0 448 512');
-    svgDelete.setAttributeNS(null, 'width', svgBtnWidth);
-    svgDelete.setAttributeNS(null, 'height', svgBtnHeight);
-
-    const pathDelete = document.createElementNS(xmlns, 'path');
-    svgDelete.appendChild(pathDelete);
-    pathDelete.setAttributeNS(null, 'd', 'M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z');
-    
-    // Add event listener to btnDelete for deleting todo.
-
-
-
-    // Add Expand button
-    // <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"/></svg>
-
-    listButtons.append(listItemEdit, listItemDelete);
-    listItemTodo.appendChild(listButtons);
-    listTodos.append(listItemTodo);
+    listTodos.appendChild(listItemTodo);
   },
   editTodo: (uuid) => {
     const todo = todos[uuid];
@@ -232,6 +243,15 @@ const DomUpdater = {
     }
     
     form.setAttribute('data-purpose', type);
+  },
+  updateTodoList: () => {
+    const listTodos = document.getElementById('list-todos');
+    listTodos.innerHTML = '';
+    // Iterate through all todos, 
+    for (const todo in todos) {
+      const listItemTodo = createTodoListItem(todos[todo]);
+      listTodos.appendChild(listItemTodo);
+    }
   }
 };
 // On Script Load, let's do some basic stuff
